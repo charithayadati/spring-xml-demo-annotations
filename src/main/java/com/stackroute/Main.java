23a43018fab745eb09ede157f4e7df4fea37923e
charithayadati <51942956+charithayadati@users.noreply.github.com>
2019-07-19 11:01:00 +0530

    Update Main.java

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -8,12 +8,12 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
     public static void main(String args[])
     {
-
+        //Using ApplicationContext
         ApplicationContext context=new AnnotationConfigApplicationContext(MovieConfig.class);
         Movie movie1=context.getBean(Movie.class);
         movie1.actorDetails();
 
-
+        //using BeanFactory
         BeanFactory factory=new AnnotationConfigApplicationContext(MovieConfig.class);
         Movie movie2=factory.getBean(Movie.class);
         movie2.actorDetails();
