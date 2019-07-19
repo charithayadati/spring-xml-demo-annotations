ccc9d10b9df8b1702464876f71c19a4497396932
charithayadati <51942956+charithayadati@users.noreply.github.com>
2019-07-19 10:43:29 +0530

    Update MovieConfig.java

--- src/main/java/com/stackroute/domain/MovieConfig.java
+++ src/main/java/com/stackroute/domain/MovieConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MovieConfig
 {
+    //Creating beans using annotations
     @Bean(name = "actor")
     public Actor getActor()
     {
