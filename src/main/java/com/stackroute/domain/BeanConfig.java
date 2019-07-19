d1b12c10b4ec53a6996c3ec47529a062a48f52a8
charithayadati <51942956+charithayadati@users.noreply.github.com>
2019-07-19 10:37:28 +0530

    Update BeanConfig.java

--- src/main/java/com/stackroute/domain/BeanConfig.java
+++ src/main/java/com/stackroute/domain/BeanConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class BeanConfig
 {
+    //Creating bean using annotation and calling the methods
     @Bean(initMethod = "customInit",destroyMethod = "customDestroy")
     public BeanLifecycleDemoBean beanlifecycle() {
         return new BeanLifecycleDemoBean();
