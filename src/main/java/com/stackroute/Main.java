daa979fd131a3d0f89933540cc92ce2c920ffb2b
charithayadati <51942956+charithayadati@users.noreply.github.com>
2019-07-19 10:35:55 +0530

    Update Main.java

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
     public static void main(String args[])
     {
+        //Using ApplicationContext
         ConfigurableApplicationContext context=new AnnotationConfigApplicationContext(BeanConfig.class);
         BeanLifecycleDemoBean bean=context.getBean("beanlifecycle",BeanLifecycleDemoBean.class);
         BeanPostProcessorDemoBean beanpost=context.getBean("beanpost",BeanPostProcessorDemoBean.class);
